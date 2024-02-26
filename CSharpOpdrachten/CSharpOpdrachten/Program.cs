6cf7f8dfabca021428761e6e3570bd78d6bdf8fb
Alex Bons <36956@ma-web.nl>
2024-02-26 15:13:27 +0100

    gitignore

---
+++ CSharpOpdrachten/CSharpOpdrachten/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpOpdrachten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dit is het programma ProjectAddOpdrach");
+        }
+    }
+}
