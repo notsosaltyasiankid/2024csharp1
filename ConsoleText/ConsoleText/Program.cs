67ee8a7e89aaf4c4846eb67633dcfa1e77a61fc0
Alex Bons <36956@ma-web.nl>
2024-02-29 12:19:28 +0100

    extraOp1

---
+++ ConsoleText/ConsoleText/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleText
+{
+    using System;
+    using System.Threading;
+
+    class Program
+    {
+        static void Main()
+        {
+            string text = "Hello World";
+            ConsoleColor originalColor = Console.ForegroundColor;
+
+            foreach (char letter in text)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(letter);
+                Thread.Sleep(200); // Wacht 200 milliseconden tussen elke letter
+            }
+
+            Console.ForegroundColor = originalColor; // Herstel de oorspronkelijke kleur
+            Console.WriteLine(); // Voeg een nieuwe regel toe na het weergeven van de tekst
+        }
+    }
+}
